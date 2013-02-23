1f1366631bb80dcbaa5ad7ce0067fbf69578a50d
chao <chao@snappytv.com>
2013-02-22 16:24:24 -0800

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@
 
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
-  has_secure_password
+  
 
   before_save { |user| user.email = email.downcase }
 
@@ -22,4 +22,5 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+  has_secure_password
 end
