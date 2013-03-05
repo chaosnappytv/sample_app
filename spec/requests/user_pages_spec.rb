62a0b06907b060d3ee61b1b12309e27a672955bf
chao <chao@snappytv.com>
2013-03-04 16:31:43 -0800

    Fnish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -31,7 +31,6 @@ describe "User pages" do
         expect { click_button submit }.not_to change(User, :count)
       end
     end
-
     describe "with valid information" do
       before do
         fill_in "Name",         with: "Example User"
@@ -39,6 +38,7 @@ describe "User pages" do
         fill_in "Password",     with: "foobar"
         fill_in "Confirmation", with: "foobar"
       end
+
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
